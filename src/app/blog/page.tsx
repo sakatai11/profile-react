161caaf66df66caf82c6d5b218bee15c9ae7a749
taichisakai <sakai111893@gmail.com>
2024-06-12 20:35:26 +0900

    いらないコード削除

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function BlogPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h2 className="text-6xl font-bold">Home Page</h2>
+      <h2 className="text-6xl font-bold">Blog Page</h2>
     </div>
   );
 }
