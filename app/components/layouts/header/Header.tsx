a0b6c955dc40bb81f92beb4099bd2b46a2e1bca7
taichisakai <sakai111893@gmail.com>
2024-06-15 20:39:53 +0900

    リンクのアニメーション調整

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -1,38 +1,46 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import { pageLinks } from "@/data/links";
-import { topLink } from "@/data/links";
+import { pageLinks, topLink } from "@/data/links";
 import { useEffect } from "react";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
+import { useCallback } from "react";
 
 const Header: React.FC = () => {
-  const pathname: string  = usePathname();
+  const pathname = usePathname();
+
+  const getLinkClass = useCallback((path:string) => {
+    return pathname === path? 'is-active' : '';
+  },[pathname]);
 
   useEffect(() => {
-    let lastScrollTop = window.scrollY; 
+    // クライアントサイドでのみ実行
+    if (typeof window !== 'undefined') {
 
-		const handleScroll = () => {
-    // headerのスクロール位置を取得
-    const header = document.querySelector("header");
-    if (header) {
-      const currentScrollTop = window.scrollY;
+      let lastScrollTop = window.scrollY; 
   
-      if (currentScrollTop > lastScrollTop) {
-        header.classList.add("-translate-y-full");
-      } else if (currentScrollTop < lastScrollTop) {
-        // 上にスクロールした場合
-        header.classList.remove("-translate-y-full");
+      const handleScroll = () => {
+      // headerのスクロール位置を取得
+      const header = document.querySelector("header");
+      if (header) {
+        const currentScrollTop = window.scrollY;
+    
+        if (currentScrollTop > lastScrollTop) {
+          header.classList.add("-translate-y-full");
+        } else if (currentScrollTop < lastScrollTop) {
+          // 上にスクロールした場合
+          header.classList.remove("-translate-y-full");
+        }
+        lastScrollTop = currentScrollTop; // 現在のスクロール位置を記録
       }
-      lastScrollTop = currentScrollTop; // 現在のスクロール位置を記録
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+      // コンポーネントがアンマウントされるときにイベントリスナーを削除
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
     }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-		// コンポーネントがアンマウントされるときにイベントリスナーを削除
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
 
   }, []);
 
@@ -51,7 +59,7 @@ const Header: React.FC = () => {
             src="/home-img.svg"
             height={24}
             width={24}
-            alt=""
+            alt="イラスト"
           />
         </Link>
           ))
@@ -59,7 +67,7 @@ const Header: React.FC = () => {
         <div className="flex justify-center gap-7">
           {
             pageLinks.map(({name, href}) => (
-              <Link href={href} key={name}>{name}</Link>
+              <Link href={href} key={name} className={`linkNav ${getLinkClass(href)}`}>{name}</Link>
             ))
           }
         </div>
