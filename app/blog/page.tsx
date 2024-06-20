0c812d25c7a4991fe4fcdacd2c7a4b46a04b4220
taichisakai <sakai111893@gmail.com>
2024-06-20 15:57:52 +0900

    Blogのページレイアウト

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,7 @@
+import BlogMain from "@/features/blog/conponents/BlogMain";
 
 export default function BlogPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="font-spartan h1Tit">Blog</h1>
-    </div>
+    <BlogMain />
   );
 }
---
+++ features/blog/conponents/BlogMain.tsx
@@ -0,0 +1,15 @@
+const BlogMain = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="flex flex-col items-center w-auto">
+          <h1 className="font-spartan h1Tit text-center">Blog</h1>
+        </div>
+        <div className="w-full">
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default BlogMain;
