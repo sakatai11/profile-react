ddcf4efb3c0620628021a72622709d7b78947071
taichisakai <sakai111893@gmail.com>
2024-06-25 17:54:08 +0900

    profileのデータ取得位置の変更

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,4 +1,5 @@
 import { MicroCMSQueries, createClient } from "microcms-js-sdk";
+import { ProfileContents } from "@/types/cms/profile";
 import { BlogArticles } from "@/types/cms/blog";
 import { Category } from "@/types/cms/category";
 
@@ -15,6 +16,20 @@ export const microCMSClient = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || "",
   apiKey: process.env.API_KEY || ""
 });
+
+// プロフィールの取得
+export const getProfile = async () => {
+  const result = await microCMSClient.getList<ProfileContents>({
+    customRequestInit: {
+    cache: "force-cache", // キャッシュ内でデータを取得する（SSG）
+    },
+    endpoint: "profile",
+  });
+
+  return {
+    props: result.contents,
+  };
+}
 
 // 記事一覧の取得
 export const getBlogArticle = async (param: string) => {
