adfee828d3ce0877e2857a37a9144148f1c0b47c
taichisakai <sakai111893@gmail.com>
2024-07-05 13:47:19 +0900

    500ページの修正

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,13 @@
 "use client"
-import * as Internal from "@/features/500/conponents/Index"
+import * as Internal from "@/features/500/conponents/Index";
+import { useEffect } from 'react';
 
-export default function InternalPage() {
+export default function InternalPage({error} : {error: Error & { digest?: string }}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+  
   return (
     <Internal.InternalWrapper />
   );
