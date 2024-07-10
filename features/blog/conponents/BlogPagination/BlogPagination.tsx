ed26341c4d344a85c911904c95d57d924e044907
taichisakai <sakai111893@gmail.com>
2024-07-10 15:00:33 +0900

    ページネーションおレイアウト調整完了

--- features/blog/conponents/BlogPagination/BlogPagination.tsx
+++ features/blog/conponents/BlogPagination/BlogPagination.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 import { PAGE_NAVI } from "@/types/cms/setting";
+import arrow from "@/public/anker-icon.svg"
 
 type PaginationProps = {
   basePath?:string;
@@ -12,28 +14,43 @@ const BlogPagination = ({basePath,currentPage,totalCount,postlimit = PAGE_NAVI.N
   const totalPages = Math.ceil(totalCount / postlimit);
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  console.log(basePath);
-
-
   return (
-    <div>
-      <ul>
+    <nav className="my-20">
+      <ul className="flex justify-center items-center gap-5">
         {currentPage > 1 && (
           <li>
-            <Link href={`${!basePath ? '/blog' : basePath}/p/${currentPage - 1}`}>
-              ← Previous
-            </Link>
+            {currentPage - 1 === 1 ? (
+              <Link href={`/blog/${basePath}`}>
+                <Image 
+                  src={arrow}
+                  height={20}
+                  width={20}
+                  alt="arrow-left"
+                  className="rotate-90"
+                />
+              </Link>
+            ) : (
+              <Link href={`${!basePath ? '/blog' : basePath}/p/${currentPage - 1}`}>
+                <Image 
+                  src={arrow}
+                  height={20}
+                  width={20}
+                  alt="arrow-left"
+                  className="rotate-90"
+                />
+              </Link>
+            )}
           </li>
         )}
         {pages.map((p) => (
-          <li key={p}>
+          <li key={p} className={`${"flex justify-center items-center w-10 h-auto aspect-square"} ${currentPage !== p ? "hover:text-skyblue" : "rounded-full border border-[#0094FF]"}`}>
             {currentPage !== p ? (
               p === 1 ? (
                 <Link href={`/blog/${basePath}`}>
                   {p}
                 </Link>
               ) : (
-                <Link href={`${!basePath ? '/blog' : basePath}/p/${p}`}>
+                <Link href={`$s{!basePath ? '/blog' : basePath}/p/${p}`}>
                   {p}
                 </Link>
               )
@@ -45,12 +62,18 @@ const BlogPagination = ({basePath,currentPage,totalCount,postlimit = PAGE_NAVI.N
         {currentPage < totalPages && (
           <li>
             <Link href={`${!basePath ? '/blog' : basePath}/p/${currentPage + 1}`}>
-              Next →
+            <Image 
+              src={arrow}
+              height={20}
+              width={20}
+              alt="arrow-right"
+              className="-rotate-90"
+              />
             </Link>
           </li>
         )}
       </ul>
-    </div>
+    </nav>
   );
 
 };
