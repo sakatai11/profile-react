65093b2244988b7435d1c08edde662bc9eec1edc
taichisakai <sakai111893@gmail.com>
2024-07-11 01:09:03 +0900

    backdrop-blurを適用、イベントを削除

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -2,43 +2,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { pageLinks, topLink } from "@/data/links";
-import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const Header = () => {
   const pathname = usePathname();
-
-  useEffect(() => {
-    // クライアントサイドでのみ実行
-    if (typeof window !== 'undefined') {
-
-      let lastScrollTop = window.scrollY; 
-  
-      const handleScroll = () => {
-      // headerのスクロール位置を取得
-      const header = document.querySelector("header");
-      const currentScrollTop = window.scrollY;
-      const scrollThreshold = 5; // スクロールの閾値を設定
-
-      if (header) {
-        if (currentScrollTop > lastScrollTop) {;
-          header.classList.add("-translate-y-full");
-        } else if (currentScrollTop - scrollThreshold < lastScrollTop) {
-          // 上にスクロールした場合
-          header.classList.remove("-translate-y-full");
-        } 
-        lastScrollTop = currentScrollTop; // 現在のスクロール位置を記録
-      }
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      // コンポーネントがアンマウントされるときにイベントリスナーを削除
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }
-
-  }, []);
 
   // トップページではHeaderを表示しない
   if (pathname === '/') {
@@ -46,7 +13,7 @@ const Header = () => {
   }
 
   return (
-    <header className="py-5 md:py-3 px-7 md:px-5 w-full fixed z-50 transition-transform duration-300">
+    <header className="py-5 md:py-3 px-7 md:px-5 w-full fixed z-50 backdrop-blur-md">
       <div className="flex justify-between items-center">
         {
           topLink.map(({name, href}) => (
