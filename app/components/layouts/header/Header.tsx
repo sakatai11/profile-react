e3ef2d7b6abd92cf06e58a4359e5ecc268ef5016
taichisakai <sakai111893@gmail.com>
2024-07-11 15:51:56 +0900

    SP端末のヘッダー動作の修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -2,10 +2,47 @@
 import Image from "next/image";
 import Link from "next/link";
 import { pageLinks, topLink } from "@/data/links";
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const Header = () => {
   const pathname = usePathname();
+
+  useEffect(() => {
+    // クライアントサイドでのみ実行
+    if (typeof window !== 'undefined') {
+
+      let lastScrollTop = window.scrollY; 
+  
+      const handleScroll = () => {
+      // headerのスクロール位置を取得
+      const header = document.querySelector("header");
+      const currentScrollTop = window.scrollY;
+
+      if (header) {
+        if (currentScrollTop > lastScrollTop) {;
+          header.classList.add("-translate-y-full");
+        } else if (currentScrollTop < lastScrollTop) {
+          // 上にスクロールした場合
+          header.classList.remove("-translate-y-full");
+        } 
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+      }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+      window.addEventListener("touchstart", handleScroll);
+      window.addEventListener("touchmove", handleScroll);
+
+      // コンポーネントがアンマウントされるときにイベントリスナーを削除
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+        window.removeEventListener("touchstart", handleScroll);
+        window.removeEventListener("touchmove", handleScroll);
+      };
+    }
+
+  }, []);
 
   // トップページではHeaderを表示しない
   if (pathname === '/') {
@@ -13,7 +50,7 @@ const Header = () => {
   }
 
   return (
-    <header className="py-5 md:py-3 px-7 md:px-5 w-full fixed z-50 backdrop-blur-md">
+    <header className="py-5 md:py-3 px-7 md:px-5 w-full fixed z-50 duration-500 backdrop-blur-md">
       <div className="flex justify-between items-center">
         {
           topLink.map(({name, href}) => (
