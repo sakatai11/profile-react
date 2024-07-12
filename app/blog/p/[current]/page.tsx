4db3d99c230c747f5a23d7f58b802ebbcf0c20b4
taichisakai <sakai111893@gmail.com>
2024-07-12 21:36:10 +0900

    備考削除

--- app/blog/p/[current]/page.tsx
+++ app/blog/p/[current]/page.tsx
@@ -18,9 +18,6 @@ export async function generateStaticParams() {
   const totalPages = Math.ceil(blogs.totalCount / PAGE_NAVI.NEW_LIST_LIMIT )
 
   // パラメータを生成
-  // return [...Array(totalPages)].map((_, i) => ({
-  //   current: (i + 1).toString(), // 数値を文字列に変換
-  // }));
   return [...Array(totalPages)].map((_, i) => {
     const currentPage = (i + 1).toString();
     // 各カテゴリの最初のページを除外
