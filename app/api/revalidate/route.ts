ee828d6995e6d896452d9d9251a72c2b9211afcf
taichisakai <sakai111893@gmail.com>
2024-08-14 23:47:32 +0900

    キャッシュを取得するタグを修正

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
   }
 
   // cacheの削除、再検証
-  const tag = request.nextUrl.searchParams.get('blog');
+  const tag = request.nextUrl.searchParams.get('tag');
 
   if (!tag)
     return NextResponse.json({ message: 'No tag provided' }, { status: 400 });
