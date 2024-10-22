7da90cda6b0527a64e6f845172f6aca57d97b5b7
taichisakai <sakai111893@gmail.com>
2024-10-22 22:01:19 +0900

    On-demand ISRの修正

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,20 +4,18 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   // API Keyの検証
-  const apiKey = request.headers.get('X-WEBHOOK-API-KEY');
-
-  if (!secureCompare(apiKey!, process.env.WEBHOOK_API_KEY!)) {
+  const apiKey =
+    request.headers.get('X-WEBHOOK-API-KEY') ||
+    request.headers.get('x-webhook-api-key');
+  if (!apiKey || !secureCompare(apiKey, process.env.WEBHOOK_API_KEY!)) {
     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
   }
 
-  // cacheの再検証
+  // Cacheの再検証
   const tag = request.nextUrl.searchParams.get('tag');
-
   if (!tag) {
     return NextResponse.json({ message: 'No tag provided' }, { status: 400 });
   }
-
   revalidateTag(tag);
-
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
