4b2840d8628b000a742ca704b8f6d740cdc69bf9
taichisakai <sakai111893@gmail.com>
2024-10-28 17:40:29 +0900

    ログ確認

--- app/_libs/utils.ts
+++ app/_libs/utils.ts
@@ -19,6 +19,7 @@ export const createTableOfContents = (richText: string): TocProps[] => {
 };
 
 export const secureCompare = (apiKey: string) => {
+  console.log('api:', apiKey);
   console.log('Env Variable:', process.env.X_MICROCMS_SIGNATURE);
   if (
     typeof apiKey !== 'string' ||
