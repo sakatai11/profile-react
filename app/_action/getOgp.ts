2e7aae65ca40003241ba682b9bb57bc85ae530f6
Sakai Taichi <130420550+sakatai11@users.noreply.github.com>
2025-05-26 23:13:49 +0900

    ファビコン取得できうように修正

--- app/_action/getOgp.ts
+++ app/_action/getOgp.ts
@@ -1,28 +1,45 @@
 'use server';
 
 import ogs from 'open-graph-scraper';
-import { OgpResult } from '@/types/ogp/seoOgp';
+
+export type OgpResult = {
+  title: string;
+  image: string;
+  domain: string;
+  url: string;
+  favicon: string;
+};
 
 export async function getOgp(url: string): Promise<OgpResult> {
+  const domain = new URL(url).hostname;
+  const defaultFavicon = new URL('/favicon.ico', url).toString();
+
   try {
     const { result } = await ogs({ url });
-    return {
-      title: (result.ogTitle as string) ?? '',
-      image:
-        Array.isArray(result.ogImage) && result.ogImage.length > 0
-          ? (result.ogImage[0].url as string)
-          : typeof result.ogImage === 'object' && 'url' in result.ogImage
-          ? (result.ogImage.url as string)
-          : '',
-      domain: new URL(url).hostname,
-      url,
-    };
+    const title = (result.ogTitle as string) ?? '';
+    const image =
+      Array.isArray(result.ogImage) && result.ogImage.length > 0
+        ? (result.ogImage[0].url as string)
+        : typeof result.ogImage === 'object' && 'url' in result.ogImage
+        ? (result.ogImage.url as string)
+        : '';
+    let favicon = defaultFavicon;
+    if ('favicon' in result) {
+      if (Array.isArray(result.favicon) && result.favicon.length > 0) {
+        favicon = result.favicon[0] as string;
+      } else if (typeof result.favicon === 'string') {
+        favicon = result.favicon;
+      }
+    }
+
+    return { title, image, domain, url, favicon };
   } catch (error) {
     return {
       title: '',
       image: '',
-      domain: '',
+      domain,
       url,
+      favicon: defaultFavicon,
     };
   }
 }
